67a7f157f878410e45cc59150f748317a275b890
KonstantinMukhin <kostik.mukhin.1995@gmail.com>
2022-09-16 18:51:00 +0300

    add methods onincrement and ondecrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,23 +13,13 @@ const Counter = (props) => {
         classes += value === 0 ? 'bg-warning' : 'bg-primary';
         return classes;
     };
-
-    const handleIncrement = () => {
-        console.log('handleIncrement')
-        // setValue((prevState) => prevState + 1);
-    };
-
-    const handleDecrement = () => {
-        console.log('handleDecrement')
-        // setValue((prevState) => prevState - 1);
-    };      
-
+    
     return (
         <div>
             <span>{props.name}</span>
             <span className={getBageClasses()}>{formatValue()}</span>
-            <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
-            <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>-</button>
+            <button className="btn btn-primary btn-sm m-2" onClick={() => props.onIncrement(props.id)}>+</button>
+            <button className="btn btn-primary btn-sm m-2" onClick={() => props.onDecrement(props.id)}>-</button>
             <button className="btn btn-danger btn-sm m-2" onClick={() => props.onDelete(props.id)}>Delete</button>
         </div>
     );
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -3,10 +3,10 @@ import Counter from "./counter";
 
 const CountersList = () => {
     const initialState = [
-        {id: 0, value: 0, name: "Плинтус"},
-        {id: 1, value: 0, name: "Угол внутренний"},
-        {id: 2, value: 0, name: "Угол внешний"},
-        {id: 3, value: 0, name: "Соединитель"},
+        {id: 0, value: 1, name: "Плинтус"},
+        {id: 1, value: 2, name: "Угол внутренний"},
+        {id: 2, value: 3, name: "Угол внешний"},
+        {id: 3, value: 4, name: "Соединитель"},
         {id: 4, value: 0, name: "Торцевая пара"},
     ];
 
@@ -21,17 +21,23 @@ const CountersList = () => {
         setCounters(initialState);
     };
 
-    const handleUpdeted = () => {
-        const updetedState = [
-            {id: 0, value: 1, name: "Плинтус"},
-            {id: 1, value: 2, name: "Угол внутренний"},
-            {id: 2, value: 3, name: "Угол внешний"},
-            {id: 3, value: 4, name: "Соединитель"},
-            {id: 4, value: 0, name: "Торцевая пара"},
-        ];
+    const handleIncrement = (id) => {
+        setCounters(counters.map(count => {
+            if (count.id === id) {
+                count.value += 1
+            }
+            return count
+        }));
+    };
 
-        setCounters(updetedState)
-    }
+    const handleDecrement = (id) => {
+        setCounters(counters.map(count => {
+            if (count.id === id) {
+                count.value -= 1
+            }
+            return count
+        }));    
+    };
 
     return (
         <>
@@ -40,10 +46,11 @@ const CountersList = () => {
                     key={count.id}
                     {...count}
                     onDelete={handleDelete}
+                    onIncrement={handleIncrement}
+                    onDecrement={handleDecrement}
                 />
             ))}
             <button className="btn btn-primary m-2 btn-sm" onClick={handleReset}>Сброс</button>
-            <button className="btn btn-primary m-2 btn-sm" onClick={handleUpdeted}>Обновить состояние</button>
         </>
     )
 }
